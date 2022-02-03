1b2b68efad03db884c94594f4a7360f2b40fdda3
C-CalvinNguyen <chicalvin.nguyen@georgebrown.ca>
2022-02-03 15:00:34 -0500

    removed config info

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     // JWT Secret Key to sign tokens
-    jwtSecret: 'music-player-app-capstone-3078',
+    jwtSecret: '',
     // Path to database
-    db: 'mongodb+srv://capstone_users:comp3078_access@comp3123.bxzhg.mongodb.net/comp3078_capstone?retryWrites=true&w=majority'
+    db: ''
 };
